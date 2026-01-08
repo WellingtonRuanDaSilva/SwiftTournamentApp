ee2497085b74e7df5945ad163ecfa722a053ef28
Wellington Ruan da Silva <wellington_alcides@hotmail.com>
2026-01-08 09:21:31 -0300

    Initial Commit

---
+++ SwiftTournamentApp/SwiftTournamentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTournamentAppApp.swift
+//  SwiftTournamentApp
+//
+//  Created by Wellington Ruan da Silva on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTournamentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
